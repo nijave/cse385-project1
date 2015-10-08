d5a12f9d42318d410f644480204cbd46ce16d01c
Brandon Wilson <wilso199@miamioh.edu>
2015-10-08 10:36:01 -0400

    Modified setup.py so that project will build

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 setup(
 	name = '385 FlaskApp',
 	version = '0.1dev',
+	author = 'Nick Venenga, Brandon Wilson, Blake Wasung',
+	author_email = 'wilso199@miamioh.edu',
 	packages = ['app',],
 	license = '',
-	long_description = open('README.md').read(),
+	long_description = open('README.txt').read(),
 )
