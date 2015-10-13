f5169ed23f6417f61a4a50dc11505f88187109e1
Brandon Wilson <wilso199@miamioh.edu>
2015-10-13 16:14:10 -0400

    Removed all files and combined it in to run.py

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
 #! venv/bin/python
-from app import app
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
 app.run(debug = True)
 
+@app.route('/')
+@app.route('/index')
+def index():
+        user = {'nickname': 'Brandon'}
+        posts = [
+                {
+                'author': {'nickname': 'John'},
+                'body': 'Beautiful day in Portland!'
+                },
+                {
+                'author': {'nickname': 'Susan'},
+                'body': 'The Avengers movie was so cool!'
+                }
+        ]
+        return render_template('index.html', title = 'Home', user = user, posts = posts)
