5535239b9ddbeec15b6ed9b24824995e7e1832fd
Nick Venenga <nijave@gmail.com>
2015-10-13 16:32:03 -0400

    Added basic CAS support

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 #! venv/bin/python
+import flask
+import flask_cas
 from flask import Flask
 from flask import render_template
+from flask_cas import CAS
+from flask_cas import login_required
 
 app = Flask(__name__)
-app.run(debug = True)
 
 @app.route('/')
 @app.route('/index')
@@ -20,3 +23,14 @@ def index():
                 }
         ]
         return render_template('index.html', title = 'Home', user = user, posts = posts)
+
+@app.route('/login1/')
+def route_login():
+    CAS.login()
+
+cas = CAS()
+cas.init_app(app)
+
+app.config['CAS_SERVER'] = 'https://muidp.miamioh.edu'
+app.config['CAS_AFTER_LOGIN'] = 'route_test'
+app.run(debug = True, host='0.0.0.0')
