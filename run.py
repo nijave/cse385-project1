814269f6c151a775f1b69fe119de8729f880bf81
Nick Venenga <nijave@gmail.com>
2015-10-13 17:04:11 -0400

    CAS support tenatively working

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask import render_template
 from flask_cas import CAS
 from flask_cas import login_required
+import os
 
 app = Flask(__name__)
 
@@ -23,15 +24,19 @@ def index():
         ]
         return render_template('index.html', title = 'Home', user = user, posts = posts)
 
-@app.route('/login1/')
-def route_login():
-    CAS.login()
+
+@app.route('/test/')
+@login_required
+def route_test():
+	#something
+	
 
 cas = CAS()
 cas.init_app(app)
 
 app.config['CAS_SERVER'] = 'https://muidp.miamioh.edu'
 app.config['CAS_AFTER_LOGIN'] = 'route_test'
+app.secret_key = os.environ.get('SECRET_KEY', None)
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0')
