cb7ded0d3a1e33cb2ff900250da6964a7ccceb60
Nick Venenga <nijave@gmail.com>
2015-11-17 19:54:37 -0500

    Added some more queries for adding and updating passwords

--- DOCS/Queries.sql
+++ DOCS/Queries.sql
@@ -30,3 +30,24 @@ SELECT COUNT(*) as Changes, UID as User
 FROM PASSWORD_CHANGE_HISTORY
 GROUP BY UID
 ORDER BY Changes DESC;
+
+-- Add a new user to the database
+INSERT INTO USERS (UID, DATE_ADDED, ACTIVE) VALUES('uniqueID', NOW(), 1);
+
+-- Add a user to an existing groups as viewer (V)
+INSERT INTO USER_GROUPS (UID, GID, DATE_ADDED, ROLE) 
+	VALUES('uniqueID', 1, NOW(), 'V');
+
+-- Add a user to an existing group as editor (E)
+INSERT INTO USER_GROUPS (UID, GID, DATE_ADDED, ROLE) 
+	VALUES('uniqueID', 1, NOW(), 'E');
+	
+-- Add a user to an existing group as owner (O)
+INSERT INTO USER_GROUPS (UID, GID, DATE_ADDED, ROLE) 
+	VALUES('uniqueID', 1, NOW(), 'O');	
+	
+-- Add a new password
+CALL insertPassword('uniqueID', 'Title', 'Description', 'username', 'password');
+
+-- Update a password
+CALL updatePassword('uniqueID', 1, 'new-password');
